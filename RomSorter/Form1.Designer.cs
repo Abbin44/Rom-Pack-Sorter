ec8411ed4a76662c8ae9fe0fd117900a9a2e2dc2
Abbin44 <abbin44@gmail.com>
2020-09-09 16:21:23 +0200

    Initial Commit

---
+++ RomSorter/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace RomSorter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sortBtn = new System.Windows.Forms.Button();
+            this.filePathBox = new System.Windows.Forms.TextBox();
+            this.browseDir = new System.Windows.Forms.Button();
+            this.regionMenu = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sortBtn
+            // 
+            this.sortBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.sortBtn.Location = new System.Drawing.Point(85, 113);
+            this.sortBtn.Name = "sortBtn";
+            this.sortBtn.Size = new System.Drawing.Size(75, 23);
+            this.sortBtn.TabIndex = 0;
+            this.sortBtn.Text = "Sort";
+            this.sortBtn.UseVisualStyleBackColor = true;
+            this.sortBtn.Click += new System.EventHandler(this.sortBtn_Click);
+            // 
+            // filePathBox
+            // 
+            this.filePathBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.filePathBox.Location = new System.Drawing.Point(12, 41);
+            this.filePathBox.Name = "filePathBox";
+            this.filePathBox.Size = new System.Drawing.Size(226, 20);
+            this.filePathBox.TabIndex = 1;
+            // 
+            // browseDir
+            // 
+            this.browseDir.Location = new System.Drawing.Point(15, 12);
+            this.browseDir.Name = "browseDir";
+            this.browseDir.Size = new System.Drawing.Size(96, 23);
+            this.browseDir.TabIndex = 2;
+            this.browseDir.Text = "Browse Directory";
+            this.browseDir.UseVisualStyleBackColor = true;
+            this.browseDir.Click += new System.EventHandler(this.browseDir_Click);
+            // 
+            // regionMenu
+            // 
+            this.regionMenu.Items.AddRange(new object[] {
+            "Europe",
+            "USA",
+            "Japan",
+            "Others"});
+            this.regionMenu.Location = new System.Drawing.Point(117, 81);
+            this.regionMenu.Name = "regionMenu";
+            this.regionMenu.Size = new System.Drawing.Size(121, 21);
+            this.regionMenu.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select region to keep";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(114, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Or enter file path manually";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 148);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.regionMenu);
+            this.Controls.Add(this.browseDir);
+            this.Controls.Add(this.filePathBox);
+            this.Controls.Add(this.sortBtn);
+            this.Name = "Form1";
+            this.Text = "Rom Pack Sorter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sortBtn;
+        private System.Windows.Forms.TextBox filePathBox;
+        private System.Windows.Forms.Button browseDir;
+        private System.Windows.Forms.ComboBox regionMenu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RomSorter/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RomSorter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string filePath;
+        private void browseDir_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+
+            // Show the FolderBrowserDialog.  
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                filePath = folderDlg.SelectedPath;
+                filePathBox.Text = filePath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+            }
+        }
+        private void sortBtn_Click(object sender, EventArgs e)
+        {
+            if (filePathBox.Text.Length == 0)
+            {
+                MessageBox.Show("Please enter a file path", "Error");
+                return;
+            }
+
+            if (regionMenu.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a region", "Error");
+                return;
+            }
+
+            string region = regionMenu.SelectedItem.ToString();
+
+            switch (region)
+            {
+                case "Europe":
+                    RunScan("E");
+                    break;
+                case "USA":
+                    RunScan("U");
+                    break;
+                case "Japan":
+                    RunScan("J");
+                    break;
+                case "Others":
+                    RunScan("O");
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+        }
+
+        public void RunScan(string region)
+        {
+            List<string> regions = new List<string>();
+            List<string> allRegs = new List<string>();
+
+            bool other = false;
+            bool keep = false;
+
+            if (region == "E")
+            {
+                regions.Add("(E)");
+                regions.Add("(EU)");
+                regions.Add("(EUR)");
+                regions.Add("(Europe)");
+            }
+            else if(region == "U")
+            {
+                regions.Add("(U)");
+                regions.Add("(US)");
+                regions.Add("(USA)");
+            }
+            else if(region == "J")
+            {
+                regions.Add("(J)");
+                regions.Add("(JP)");
+                regions.Add("(Japan)");
+            }
+            else if(region == "O")
+            {
+                other = true;
+                allRegs.Add("(E)");
+                allRegs.Add("(EU)");
+                allRegs.Add("(EUR)");
+                allRegs.Add("(Europe)");
+                allRegs.Add("(U)");
+                allRegs.Add("(US)");
+                allRegs.Add("(USA)");
+                allRegs.Add("(J)");
+                allRegs.Add("(JP)");
+                allRegs.Add("(Japan)");
+            }
+
+            filePath = filePathBox.Text;
+            DirectoryInfo di = new DirectoryInfo(filePath);
+
+            foreach (FileInfo file in di.EnumerateFiles())
+            {
+                if(other == false)
+                {
+                    keep = regions.Any(file.Name.Contains);
+                    if (keep == false)
+                    {
+                        file.Delete();
+                    }
+                }
+                else if(other == true)
+                {
+                    keep = allRegs.Any(file.Name.Contains);
+                    if (keep == true)
+                    {
+                        file.Delete();
+                    }
+                }
+            }
+        }
+
+    }
+}
